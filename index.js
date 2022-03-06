c6d96406dc55e8c9cf254125928ff3716e853576
Michael Emerson <emerson.michael12@gmail.com>
2022-03-05 18:45:49 -0600

    put everything in try catch :) (def good practice)

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const {v4: uuidv4} = require('uuid');
 const axios = require('axios').default;
 require('dotenv').config()
 
+const sendMessage = async (text) => {
+    await axios.post(`https://api.groupme.com/v3/groups/85327648/messages?token=${process.env.GROUPME_TOKEN}`, {
+        message: {
+            source_guid: uuidv4(),
+            text: text
+        }
+    }, {
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+}
+
 async function main() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
@@ -17,52 +30,21 @@ async function main() {
     await page.click("#idSIButton9");
     await page.waitForSelector("#bs-example-navbar-collapse-1 > ul:nth-child(1) > li:nth-child(2) > a");
     await page.goto("https://get.cbord.com/wpi/full/food_merchant.php?ID=014fef42-dd08-4799-88a0-487c402481b4");
-    await page.waitForSelector("#food_merchant_menu_div > div > ul");
-    const milkshakeOption = await page.$x("//a[contains(text(),'Milk Shake')]");
-    await milkshakeOption[0].click();
-    const milkshakeMenu = await page.$(`[class="tab-pane active"]`);
-    if (milkshakeMenu && (await (await milkshakeMenu.getProperty('firstElementChild')).getProperty('childElementCount'))._remoteObject.value > 0) {
-        console.log("Milkshake is ready!");
-        await axios.post(`https://api.groupme.com/v3/groups/85327648/messages?token=${process.env.GROUPME_TOKEN}`, {
-            message: {
-                source_guid: uuidv4(),
-                text: "Milkshake is ready!"
-            }
-        }, {
-            headers: {
-                "Content-Type": "application/json"
-            }
-        });
-    } else {
-        console.log("Milkshake is not ready!");
-        await axios.post(`https://api.groupme.com/v3/groups/85327648/messages?token=${process.env.GROUPME_TOKEN}`, {
-            message: {
-                source_guid: uuidv4(),
-                text: "Milkshake is not ready!"
-            }
-        }, {
-            headers: {
-                "Content-Type": "application/json"
-            }
-        });
+    try {
+        await page.waitForSelector("#food_merchant_menu_div > div > ul");
+        const milkshakeOption = await page.$x("//a[contains(text(),'Milk Shake')]");
+        await milkshakeOption[0].click();
+        const milkshakeMenu = await page.$(`[class="tab-pane active"]`);
+        if (milkshakeMenu && (await (await milkshakeMenu.getProperty('firstElementChild')).getProperty('childElementCount'))._remoteObject.value > 0) {
+            await sendMessage("Milkshake is ready!");
+        } else {
+            await sendMessage("Milkshake is not ready!");
+        }
+    } catch (e) {
+       await sendMessage("Error: No menu found");
     }
     await browser.close();
 }
 
 
 main();
-
-async function test() {
-    await axios.post(`https://api.groupme.com/v3/groups/85327648/messages?token=${process.env.GROUPME_TOKEN}`, {
-        message: {
-            source_guid: uuidv4(),
-            text: "Milkshake is not ready!"
-        }
-    }, {
-        headers: {
-            "Content-Type": "application/json"
-        }
-    });
-}
-
-// test();
